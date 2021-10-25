46eeb2b5d569e96ff21ae6aecdb45716dfb930d7
Andreas <saerdna.petersen@gmail.com>
2021-10-25 11:18:10 +0200

    get axios method

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const baseUrl = "https://par-music-rest.azurewebsites.net/MusicRecord"
+Vue.createApp({
+    data() {
+        return {
+            music_records: []
+        };
+    },
+    async created() {
+        this.get_records();
+    },
+    methods: {
+        async get_records() {
+            const response = await axios.get(baseUrl);
+            this.music_records = await response.data
+            console.log(this.music_records);
+        }
+    },
+}).mount("#app");
